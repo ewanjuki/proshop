2e5ee0e5fdbcc6e24ea14947178c4d87de2ffd32
Edwin Wanjuki <edwinwanjuki@gmail.com>
2023-03-13 16:44:30 +0300

    payment page

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
+  CART_SAVE_PAYMENT_METHOD,
   CART_SAVE_SHIPPING_ADDRESS,
 } from "../constants/cartConstants";
 
@@ -29,8 +30,15 @@ export const removeFromCart = (id) => (dispatch, getState) => {
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
 
+
 export const saveShippingAddress = (data) => (dispatch) => {
   dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
 
-  localStorage.setItem("shippingAddress", JSON.stringify(data));
+  localStorage.setItem("saveShippingAddress", JSON.stringify(data));
+};
+
+export const savePaymentMethod = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_PAYMENT_METHOD, payload: data });
+
+  localStorage.setItem("paymentMethod", JSON.stringify(data));
 };

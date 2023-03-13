7a9c707355a7016e3261b50a110ad26989e413ec
Edwin Wanjuki <edwinwanjuki@gmail.com>
2023-03-13 05:08:17 +0300

    checkout steps component

--- frontend/src/pages/Shipping.js
+++ frontend/src/pages/Shipping.js
@@ -4,6 +4,7 @@ import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from "../actions/cartActions";
 
 function Shipping() {
@@ -26,6 +27,7 @@ function Shipping() {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
